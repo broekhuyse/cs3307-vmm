0abd396764f5c24a435d53e9dad4f0b9b13d742d
Matt Mombourquette <mmombou@uwo.ca>
2020-11-11 19:06:33 -0500

    formatting

--- src/Interfaces/AdminInterface.cpp
+++ src/Interfaces/AdminInterface.cpp
@@ -41,9 +41,9 @@ void AdminInterface::ProductDisplay()
 			<< "QTY: " << std::left << std::setw(10) << pCollection->getProductList().at(i).getQuantity() << std::left << std::setw(10) << pCollection->getProductList().at(i).getCategory() << SHOPRIGHT << std::endl;
 	}
 
-	cout << endl;
+	cout << SHOPTOP << endl;
 }
-cout << SHOPTOP << endl;
+
 
 // interface that displays appropriate prompts and user inputs that relate to the different admin functions
 void AdminInterface::AdminPrompt()
